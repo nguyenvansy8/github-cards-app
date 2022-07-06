e3405c7772160d8c0c2f60246e40589ee6358e65
NguyenVanSy <vansy0975316291@gmail.com>
2022-07-06 10:38:33 +0700

    github-cards-app

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,75 @@
 import * as React from 'react';
 
-export function App({ initialData }) {
-  const [count, setCount] = React.useState(0);
+const CardList = (props) => (
+  <div>
+    {props.profiles.map(profile => <Card {...profile}/>)}
+  </div>
+)
+
+class Addcard extends React.Component{
+  // nameInput = React.createRef();
+  state = {name: ''};
+    handleSubmit = async (event) => {
+      event.preventDefault();
+      const respond = await axios.get(`https://api.github.com/users/${this.state.name}`);
+      this.props.onSubmit(respond.data);
+      this.setState({name: ''});
+    }
+    
+  render(){
+    return(
+      <form onSubmit = {this.handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="GitHub name" 
+          value = {this.state.name}
+          onChange = {event => this.setState({name: event.target.value})}
+          required/>
+        <button>ADD</button>
+      </form>
+    );
+  }
+}
+
+class Card extends React.Component{
+  render(){
+    const profile = this.props
+    return(
+      <div className="github-profile" style={{ margin: "1rem"}}>
+        <img src={profile.avatar_url}/>
+        <div className="info" style={{ display: "inline-block", marginLeft: 10}}>
+          <div className="name" style={{ fontSize: '200%', color: 'red'}}>{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class CardsApp extends React.Component{
+  state = {
+    profiles: [],
+  };
+  
+  addNewProfile = (profileData1) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData1],
+    }));
+  };
+  
+  render(){
+    return (
+      <div>
+        <div className="header" style={{fontSize: '200%'}}>{this.props.title}</div>
+        <Addcard onSubmit = {this.addNewProfile}/>
+        <CardList profiles = {this.state.profiles}/>
+      </div>
+    );
+  }
+}
+
+export function App() {
   return (
-    <div>
-      <h1>{initialData.appName}</h1>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button title="increment" onClick={() => setCount(count + 1)}>
-        {count}
-      </button>
-    </div>
+    <CardsApp title="The GitHub Cards App" />
   );
 }
